697b89f6fbc27c7a33865c64432dfaa5e53e9b0e
Danial Raza <danialrazafb@gmail.com>
2023-12-30 19:18:30 +0100

    chore: init app (#1)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="flex justify-center text-2xl font-semibold">
+      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+    </div>
+  )
+}
