14f96c406342fcdbe34417022c1b13dd8e49e5c3
Megatank58 <tanumegatank@gmail.com>
2024-03-12 09:14:53 +0000

    feat(/): landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content flex-col lg:flex-row-reverse">
+		<div class="mockup-code bg-base-300 text-base-content border-2 border-base-100">
+			<pre><code class="text-error">fn </code><code class="text-info">factorial</code><code
+					>{'(n: int) -> int {'}</code
+				></pre>
+			<pre><code class="text-error">    if </code><code>{'n == 0 {'}</code></pre>
+			<pre><code class="text-error">        return </code><code>1;</code></pre>
+			<pre><code>{'    }'}</code></pre>
+			<pre><code class="text-error">    return </code><code
+					>n * <code class="text-info">factorial</code>(n - 1);</code
+				></pre>
+			<pre><code>{'}'}</code></pre>
+			<pre><code></code></pre>
+			<pre><code class="text-error">print</code><code>(</code><code class="text-info"
+					>factorial</code
+				><code>(5)); <code class="text-neutral">// 120</code></pre>
+		</div>
+		<div>
+			<h1 class="text-5xl font-bold">Oxido</h1>
+			<p class="py-6 w-96">
+				Oxido is a statically typed interpreted programming language for making CLI tools and
+				scripts.
+			</p>
+			<a class="btn btn-primary" href="/guide">Install</a>
+            <a class="btn btn-accent" href="/docs">Documentation</a>
+		</div>
+	</div>
+</div>
